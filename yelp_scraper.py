4974e9764e50e51c94f20c43e3d571774f365ad6
root <root@datascience-amd64.defaultdomain>
2014-11-10 22:00:26 -0800

    updated parser: sites w/no reviews -> rejects, sites w/no inspection -> unknown

--- yelp_scraper.py
+++ yelp_scraper.py
@@ -16,7 +16,6 @@ import time
 import datetime
 from nltk.corpus import stopwords
 import nltk
-from whoosh.lang.porter import stem
 
 
 def check_url_exists(ur):
@@ -29,11 +28,11 @@ def check_url_exists(ur):
         return False
 
 # returns a list of lists, each sublist being a page of reviews of a restaurant
-def get_urls():
+def get_urls(csv_rejects):
     # below 5 pages of reviews, standard
     #urls=['http://www.yelp.com/biz/chicos-pizza-san-francisco']
     # below is a quick test, only 40 reviews
-    urls=['http://www.yelp.com/biz/lucky-ocean-cafe-san-francisco']
+    #urls=['http://www.yelp.com/biz/lucky-ocean-cafe-san-francisco']
     # below url has no yelp inspections on site
     #urls=['http://www.yelp.com/biz/ocean-pearl-restaurant-san-francisco']
     #urls=['http://www.yelp.com/biz/quickly-san-francisco-12']
@@ -43,8 +42,15 @@ def get_urls():
     #below is mix of 100 and lesser scores
     #urls=['http://www.yelp.com/biz/lite-bite-san-francisco']
 
+    #below is restaurant with no reviews
+    urls=['http://www.yelp.com/biz/greenlid-studiomix-san-francisco']
+
+    #below is restaurant with no inspec socre
+    #urls=['http://www.yelp.com/biz/blue-c-sushi-san-francisco']
+
     #urls=['http://www.yelp.com/biz/chicos-pizza-san-francisco', 'http://www.yelp.com/biz/state-bird-provisions-san-francisco',
-    #        'http://www.yelp.com/biz/rickybobby-san-francisco', 'http://www.yelp.com/biz/kitchen-story-san-francisco']
+    #        'http://www.yelp.com/biz/blue-c-sushi-san-francisco', 'http://www.yelp.com/biz/greenlid-studiomix-san-francisco',
+    #        'http://www.yelp.com/biz/rickybobby-san-francisco', 'http://www.yelp.com/biz/kitchen-story-san-francisco',
     #        'http://www.yelp.com/biz/v-105-san-francisco', 'http://www.yelp.com/biz/dinosaurs-san-francisco',
     #        'http://www.yelp.com/biz/mau-san-francisco', 'http://www.yelp.com/biz/bar-tartine-san-francisco']
 
@@ -61,6 +67,14 @@ def get_urls():
         html = urllib2.urlopen(item + '?sort_by=date_desc').read()
         soup = BeautifulSoup(html)
         page = soup.find('div', {"class":"page-of-pages"})
+
+        # restaurant has no reviews, skip and add to rejects
+        if page == None:
+            title = soup.find('h1',itemprop="name")
+            name = title.text.encode("utf-8").strip(' \t\n\r').replace('|',' ')
+            csv_rejects.writerow(name)
+            continue
+
         while iter < int(page.text.split()[-1]):
             new_url = item + '?start=' + str(counter) + '&sort_by=date_desc'
             if check_url_exists(new_url):
@@ -73,28 +87,35 @@ def get_urls():
 
 # takes a url and scrapes attributes we want from the html, writes to csv
 def scrape(urls, filer, filer_real,iattrib):
+
+    # if site has no inspection
+    no_inspec = False
+    if iattrib == None:
+        no_inspec = True
+
     # each item in param_map is a list, where each item is  [INSPEC#, NEW_DATE, OLD_DATE, [6_PARAMS]]
     param_map = []
 
     # item here is (DATE, [6 PARAMS] )
     # list is ordered, so first item in the list is the newest DATE - inspection
     p = 0
-    while p < len(iattrib):
-	if p == len(iattrib) - 1:
-	    param_map.append( [p, iattrib[p][0] , str(datetime.date(1900,1,1)), iattrib[p][1] ] )
-	else:
-	    param_map.append( [p, iattrib[p][0] , iattrib[p+1][0], iattrib[p][1] ] )
-	p += 1
+    if not no_inspec:
+      while p < len(iattrib):
+	  if p == len(iattrib) - 1:
+	      param_map.append( [p, iattrib[p][0] , str(datetime.date(1900,1,1)), iattrib[p][1] ] )
+	  else:
+	      param_map.append( [p, iattrib[p][0] , iattrib[p+1][0], iattrib[p][1] ] )
+	  p += 1
 
-    # full_map is the final, each key in full_map is the numbered inspection, value is [ [6 PARAMS], REVIEW_CORPUS, RATING_AVG, review_count ]
-    full_map = {}
+      # full_map is the final, each key in full_map is the numbered inspection, value is [ [6 PARAMS], REVIEW_CORPUS, RATING_AVG, review_count ]
+      full_map = {}
 
-    unknown_map = {}
-    unknown_map[-1] = [ param_map[0][3], '', 0, 0]
+      unknown_map = {}
+      unknown_map[-1] = [ param_map[0][3], '', 0, 0]
 
-    # initiliaze dict
-    for item in param_map:
-	full_map[item[0]] = [item[3], '', 0 , 0]
+      # initiliaze dict
+      for item in param_map:
+	  full_map[item[0]] = [item[3], '', 0 , 0]
 
     review_count = 0
     for ur in urls:
@@ -112,6 +133,12 @@ def scrape(urls, filer, filer_real,iattrib):
 	name = title.text.encode("utf-8").strip(' \t\n\r').replace('|',' ')
 	total_rating = float( ratings[0]['content'] )
 
+        if reviews == None:
+            print "This should not have happened, restaurant has no reviews even though it passed first check"
+
+        # for case of no yelp inspection
+        review_agg = ''
+
 	i = 0
 	while (i < len(reviews)):
 	    rating = float( ratings[i+1]['content'] )
@@ -121,28 +148,34 @@ def scrape(urls, filer, filer_real,iattrib):
             # feature select from review_text
             review_text = select_features(review_text)
 
-            flag = True
-	    for item in param_map:
-                # this is the unknown data, no labels cuz no inspection!
-                if flag and date > item[1]:
-		    unknown_map[-1][3] += 1
-		    unknown_map[-1][1] = unknown_map[-1][1] + ' ' + review_text
-		    unknown_map[-1][2] += rating
-                    flag = False
-		if date < item[1] and date >= item[2]:
-		    full_map[item[0]][3] += 1
-		    full_map[item[0]][1] = full_map[item[0]][1] + ' ' + review_text
-		    full_map[item[0]][2] += rating 
+            if not no_inspec:
+              flag = True
+	      for item in param_map:
+                  # this is the unknown data, no labels cuz no inspection!
+                  if flag and date > item[1]:
+		      unknown_map[-1][3] += 1
+		      unknown_map[-1][1] = unknown_map[-1][1] + ' ' + review_text
+		      unknown_map[-1][2] += rating
+                      flag = False
+		  if date < item[1] and date >= item[2]:
+		      full_map[item[0]][3] += 1
+		      full_map[item[0]][1] = full_map[item[0]][1] + ' ' + review_text
+		      full_map[item[0]][2] += rating 
+            else:
+                review_agg = review_agg + ' ' + review_text
 	    i += 1
             review_count += 1
 
-    for key,val in full_map.iteritems():
-        # if it equals 0, that means there were no reviews for that time period... we lost a sample :(
-        if val[3] != 0:
-            filer.writerow( [name + ' ' + str(key),total_rating,category,price_category,review_count,key,val[2] / float(val[3]),val[1]] + val[0] ) 
+    if not no_inspec:
+      for key,val in full_map.iteritems():
+          # if it equals 0, that means there were no reviews for that time period... we lost a sample :(
+          if val[3] != 0:
+              filer.writerow( [name + ' ' + str(key),total_rating,category,price_category,review_count,key,val[2] / float(val[3]),val[1]] + val[0] ) 
 
-    if unknown_map[-1][3] != 0:
-        filer_real.writerow( [name,total_rating,category,price_category,review_count,-1,unknown_map[-1][2] / float(unknown_map[-1][3]),unknown_map[-1][1]] ) 
+      if unknown_map[-1][3] != 0:
+          filer_real.writerow( [name,total_rating,category,price_category,review_count,-1,unknown_map[-1][2] / float(unknown_map[-1][3]),unknown_map[-1][1]] ) 
+    else:
+          filer_real.writerow( [name,total_rating,category,price_category,review_count,-1,total_rating,review_agg] ) 
 
 # use 10 feature selection principles from http://cs.brown.edu/courses/csci1951-a/assignments/assignment3/
 # ahead of time so that classifier has to deal with less clutter
@@ -277,24 +310,23 @@ def scrape_inspection(ur):
 # iteratres through a list of urls, and deeper into suburls, each suburl is a page of reviews
 def main():
     f = csv.writer(open("/home/datascience/FINAL_PROJECT/yelp_predictor/dataset_yelp.csv", "w"),delimiter='|')
-    fbad = csv.writer(open("/home/datascience/FINAL_PROJECT/yelp_predictor/dataset_rejects.csv", "w"),delimiter='|')
+    fbad = csv.writer(open("/home/datascience/FINAL_PROJECT/yelp_predictor/dataset_rejects.csv", "w"))
+    fbad.writerow(["rejects"])
     freal = csv.writer(open("/home/datascience/FINAL_PROJECT/yelp_predictor/dataset_unknown.csv", "w"),delimiter='|')
     f.writerow(["name","total_rating","category","price_category","number_reviews","inspec_period","period_rating","review_text", 
                 "number_inspections","health_score","number_violations","inspec_type","inspec_vio","verdict"])
     freal.writerow(["name","total_rating","category","price_category","number_reviews","inspec_period","period_rating","review_text", 
                 "number_inspections","health_score","number_violations","inspec_type","inspec_vio","verdict"])
-    for items in get_urls():
+    for items in get_urls(fbad):
         inter = items[0].split('?', 1)[0] 
         inspection_url = inter.replace('biz', 'inspections')
         if check_url_exists(inspection_url) == True:
             inspectors = scrape_inspection(inspection_url)
             scrape(items, f, freal, inspectors)
         else:
-            # if there is no health ratings on yelp, skip the restaurant entirely
-            # write rejections to csv
-            fbad.writerow("rejects")
-            fbad.writerow(items[0])
-            continue
+            # if there is no health ratings on yelp, add restaurant to test set
+            inspectors = None
+            scrape(items, f, freal, inspectors)
         print 'Site done'
     print 'Operation completed...!'
 
