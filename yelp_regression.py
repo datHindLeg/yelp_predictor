d54d8655ea5f672a94b69fdf7c732bc9f314f6a7
root <root@datascience-amd64.defaultdomain>
2014-11-10 23:14:25 -0800

    using bigram feature extraction/linear regression

--- yelp_regression.py
+++ yelp_regression.py
@@ -7,6 +7,7 @@ import matplotlib.pyplot as plt
 import math
 import pandas as pd
 from sklearn import linear_model
+from sklearn import ensemble
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.feature_selection import SelectKBest
@@ -74,10 +75,10 @@ def main():
         print name + ' => ' + str(int(math.floor(value))) + '\n'
 
     # evaluate mean square error using validation set
-    print("Residual sum of squares: %.2f" % rms)
+    print("Residual mean error: %.2f" % rms)
 
     # Explained variance score: 1 is perfect prediction
-    print('Variance score: %.2f' % variance)
+    print('Variance score: %.2f' % math.fabs(variance))
 
 
 # *****************************************************************************************************
@@ -91,34 +92,31 @@ def build_feature_extractors(train_text, validation_text):
 
     # 1. starting with a simple bag of words model
     # training set
-    vectorizer_bag = CountVectorizer(min_df=1)
-    X_train_counts = vectorizer_bag.fit_transform(train_text)
+    #vectorizer = CountVectorizer(min_df=1)
+
+    # 2. bi-gram
+    vectorizer = CountVectorizer(ngram_range=(1, 2), token_pattern=r'\b\w+\b', min_df=1)
+
+    # 3. tri-gram
+    #vectorizer = CountVectorizer(ngram_range=(1, 3), token_pattern=r'\b\w+\b', min_df=1)
+
+    # 4. TODO: dependency parsing with a fast DP like maltparser.
+
+    X_train_counts = vectorizer.fit_transform(train_text)
+
     tfidf_transformer = TfidfTransformer()
     X_train_tfidf = tfidf_transformer.fit_transform(X_train_counts)
 
+    # validation set
+    Y_validation_counts = vectorizer.transform(validation_text)
+    Y_validation_tfidf = tfidf_transformer.transform(Y_validation_counts)
+
     # DEBUG: find out what features are being selected
     #print_features(X_train_tfidf)
-    #print_features(vectorizer_bag)
-
-    # validation set
-    Y_validation_counts = vectorizer_bag.transform(validation_text)
-    Y_validation_tfidf = tfidf_transformer.transform(Y_validation_counts)
+    #print_features(vectorizer)
 
     return X_train_tfidf, Y_validation_tfidf
 
-    '''
-    # 2. n-gram model, test which one is best by passing in diff values for n (most likely between 2-4)
-    bigram_vectorizer = CountVectorizer(ngram_range(1,2),
-                           token_pattern=r'\b\w+\b', min_df=1)
-    X_2 = bigram_vectorizer.fit_transform(corpus)
-    
-    trigram_vectorizer = CountVectorizer(ngram_range(1,3),
-                           token_pattern=r'\b\w+\b', min_df=1)
-    X_3 = bigram_vectorizer.fit_transform(corpus)
-    
-    # 3. TODO: dependency parsing with a fast DP like maltparser.
-
-    '''
 
 # *****************************************************************************************************
 # -----------------------------------------------------------------------------------------------------
@@ -134,6 +132,25 @@ def linear_regression(model_train, train_targets, model_validation, validation_t
 
     # train the model
     regr.fit(model_train, train_targets)
+
+    return (regr.predict(model_validation), np.mean((regr.predict(model_validation) - validation_targets) ** 2), 
+            regr.score(model_validation, validation_targets))
+
+def ridge_regression(model_train, train_targets, model_validation, validation_targets):
+    regr = linear_model.Ridge(alpha=1.0)
+
+    # train the model
+    regr.fit(model_train, train_targets)
+
+    return (regr.predict(model_validation), np.mean((regr.predict(model_validation) - validation_targets) ** 2), 
+            regr.score(model_validation, validation_targets))
+
+# TODO: doesn't work now
+def random_forest_regression(model_train, train_targets, model_validation, validation_targets):
+    regr = ensemble.RandomForestRegressor()
+
+    # train the model
+    regr.fit(model_train.toarray(), train_targets.toarray())
 
     return (regr.predict(model_validation), np.mean((regr.predict(model_validation) - validation_targets) ** 2), 
             regr.score(model_validation, validation_targets))
