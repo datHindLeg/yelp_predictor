f622af68bd9a86f8c0be465187010bb90217c0d4
root <root@datascience-amd64.defaultdomain>
2014-12-09 21:50:26 -0800

    cleaning up stuff

--- sql_scripts/import_csv.sql
+++
@@ -1,9 +0,0 @@
-BULK INSERT yelp_project.dbo.dataset_yelp
-FROM 'YOUR PATH TO CSV'
-WITH
-(
-  FIRSTROW = 2,
-  FIELDTERMINATOR = '|',  --CSV field delimiter
-  ROWTERMINATOR = '\n',   --Use to shift the control to next row
-  TABLOCK
-)
